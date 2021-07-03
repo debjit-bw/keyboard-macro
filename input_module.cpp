f4c530b7382681d3d3b944062cde37a3395716aa
debjit <debjitbhowal.db@gmail.com>
2021-07-03 23:41:10 +0530

    live input detection added

---
+++ input_module.cpp
@@ -0,0 +1,75 @@
+#define UNICODE
+#include <windows.h>
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <time.h>
+
+// variable to store the HANDLE to the hook. Don't declare it anywhere else then globally
+// or you will get problems since every function uses this variable.
+HHOOK _hook;
+
+// This struct contains the data received by the hook callback. As you see in the callback function
+// it contains the thing you will need: vkCode = virtual key code.
+KBDLLHOOKSTRUCT kbdStruct;
+
+int Track(int key_stroke);
+
+// This is the callback function. Consider it the event that is raised when, in this case,
+// a key is pressed.
+LRESULT __stdcall HookCallback(int nCode, WPARAM wParam, LPARAM lParam)
+{
+    if (nCode >= 0)
+    {
+        // the action is valid: HC_ACTION.
+        if (wParam == WM_KEYDOWN)
+        {
+            // lParam is the pointer to the struct containing the data needed, so cast and assign it to kdbStruct.
+            kbdStruct = *((KBDLLHOOKSTRUCT *)lParam);
+
+            // save to file
+            Track(kbdStruct.vkCode);
+        }
+    }
+
+    // call the next hook in the hook chain. This is nessecary or your hook chain will break and the hook stops
+    return CallNextHookEx(_hook, nCode, wParam, lParam);
+}
+
+void SetHook()
+{
+    // Set the hook and set it to use the callback function above
+    // WH_KEYBOARD_LL means it will set a low level keyboard hook. More information about it at MSDN.
+    // The last 2 parameters are NULL, 0 because the callback function is in the same thread and window as the
+    // function that sets and releases the hook.
+    if (!(_hook = SetWindowsHookEx(WH_KEYBOARD_LL, HookCallback, NULL, 0)))
+    {
+        LPCWSTR a = L"Failed to install hook!";
+        LPCWSTR b = L"Error";
+        MessageBox(NULL, a, b, MB_ICONERROR);
+    }
+}
+
+void ReleaseHook()
+{
+    UnhookWindowsHookEx(_hook);
+}
+
+int Track(int key_stroke)
+{
+    std::cout << key_stroke;
+    return 0;
+}
+
+int main()
+{
+    // set the hook
+    SetHook();
+
+    // loop to keep the console application running.
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+    }
+}
